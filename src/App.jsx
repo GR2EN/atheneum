585e17e0ef0204195ed34875319252245b24d846
Almaz Kaliev <almaz.dulatovich@gmail.com>
2020-09-07 14:36:46 +0600

    delete unnecessary theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import { AuthProvider } from './Auth';
-import { Home, SignIn, SignUp } from './pages';
+import { Home, SignIn, SignOut, SignUp } from './pages';
 import PrivateRoute from './PrivateRoute';
 
 const App = () => {
   return (
     <AuthProvider>
+      <CssBaseline />
       <Router>
         <Switch>
           <PrivateRoute component={Home} exact path="/" />
+          <PrivateRoute component={SignOut} path="/sign-out" />
           <Route component={SignIn} path="/sign-in" />
           <Route component={SignUp} path="/sign-up" />
         </Switch>
